ac7565151179b44078e3608846a222ad8d34bfa3
ciaran1313 <caerfinn@gmail.com>
2016-12-26 18:56:39 -0500

    Added trampling (Not tested. Likely doesnt work properly)

--- CaptureAndTrample.hs
+++ CaptureAndTrample.hs
@@ -7,12 +7,16 @@ module CaptureAndTrample where
   import Piece
 
   capture :: Location -> BoardMap -> BoardMap
-  capture location boardMap = captureToward nextLocation (Just location) $ captureToward previousLocation (Map.lookup location boardMap >>= previousLocation) $ boardMap
+  capture location boardMap = captureToward nextLocation (\ _ -> False) (Just location) $ captureToward previousLocation (if isCapture then isStop else (\ _ -> False)) (Map.lookup location boardMap >>= previousLocation) $ boardMap
     where
-      captureToward :: (Piece -> Maybe Location) -> Maybe Location -> BoardMap -> BoardMap
-      captureToward f m_location boardMap
-        | isNothing m_location = boardMap
-        | otherwise = captureToward f (pieceAtLocation >>= f) $ Map.delete (fromJust m_location) boardMap
+
+      isCapture :: Bool
+      isCapture = fromMaybe False (isStop <$> Map.lookup location boardMap
+
+      captureToward :: (Piece -> Maybe Location) -> (Piece -> Bool) -> Maybe Location -> BoardMap -> BoardMap
+      captureToward f terminationCondition m_location boardMap
+        | isNothing m_location || terminationCondition (fromJust pieceAtLocation) = boardMap
+        | otherwise = captureToward f terminationCondition (pieceAtLocation >>= f) $ Map.delete (fromJust m_location) boardMap
         where
           pieceAtLocation :: Maybe Piece
           pieceAtLocation = (>>=)(m_location)(\defloc -> Map.lookup defloc boardMap)
