22b2fa0a12cd1dd8eb105b065ec70dc8b7603e33
ciaran1313 <caerfinn@gmail.com>
2016-12-27 23:07:57 -0500

    added ability to view board from different perspectives

--- CommandLine.hs
+++ CommandLine.hs
@@ -42,37 +42,40 @@ module Main where
         print $ cutCrossSection (vis_t, n) vis_x vis_y game;
         putStrLn $ (++ ":")(show $ turnColour game);
         argv <- fmap words getLine;
-        let argc = length argv in
-          case head argv of
+        if length argv <= 1
+          then runGame oldParams
+          else
+            let argc = length argv in
+              case head argv of
 
-            "help" -> if argc == 1
-              then do {
-                putStrLn helpMessage;
-                pause;
-                runGame oldParams;
-              } else badargc;
+                "help" -> if argc == 1
+                  then do {
+                    putStrLn helpMessage;
+                    pause;
+                    runGame oldParams;
+                  } else badargc;
 
-            "move" -> if argc >= 3
-              then runGame $ newParams oldParams $ (map read . filter (/= "to") . tail :: [String] -> [Location]) argv;
-              else badargc;
+                "move" -> if argc >= 3
+                  then runGame $ newParams oldParams $ (map read . filter (/= "to") . tail :: [String] -> [Location]) argv;
+                  else badargc;
 
-            "view" -> if argc == 5
-              then runGame ((new_vis_t, new_n), new_vis_x, new_vis_y, game)
-              else badargc
-              where
-                new_vis_t, new_vis_x, new_vis_y :: Coordinate
-                new_vis_t = read $ argv !! 1
-                new_vis_x = read $ argv !! 3
-                new_vis_y = read $ argv !! 4
+                "view" -> if argc == 5
+                  then runGame ((new_vis_t, new_n), new_vis_x, new_vis_y, game)
+                  else badargc
+                  where
+                    new_vis_t, new_vis_x, new_vis_y :: Coordinate
+                    new_vis_t = read $ argv !! 1
+                    new_vis_x = read $ argv !! 3
+                    new_vis_y = read $ argv !! 4
 
-                new_n :: Int
-                new_n = read $ argv !! 2
+                    new_n :: Int
+                    new_n = read $ argv !! 2
 
-            _ -> do {
-              putStrLn $ "Please enter a valid command. (Example: \"help\")";
-              pause;
-              runGame oldParams;
-          }
+                _ -> do {
+                  putStrLn $ "Please enter a valid command. (Example: \"help\")";
+                  pause;
+                  runGame oldParams;
+              }
       } where
           badargc :: IO ()
           badargc = do {
