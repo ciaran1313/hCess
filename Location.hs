53a6f90616f4071c48b88c79055ba125be716d3c
ciaran1313 <caerfinn@gmail.com>
2017-03-23 22:59:54 -0400

    made read location throw an actual "Prelude.read: no parse" error

--- Location.hs
+++ Location.hs
@@ -15,17 +15,11 @@ module Location where
     show (Location t x y) = (toFile x) : (toRank y) : (toRomanNumeral t)
 
   instance Read Location where
-    readsPrec d r = [(readLocation r, [])]
+    readsPrec _ (file:rank:turn) = maybe [] (\location -> [(location, "")]) (Location <$> t <*> x <*> y)
       where
+          t, x, y :: Maybe Int
+          t = fromTurn (turn)
+          x = fromFile [file]
+          y = fromRank [rank]
 
-        no_parse = errorWithoutStackTrace "Prelude.read: no parse"
-
-        readLocation :: String -> Location
-        readLocation(file:rank:turn) = Location t x y
-          where
-            t, x, y :: Int
-            t = fromMaybe no_parse $ fromTurn (turn)
-            x = fromMaybe no_parse $ fromFile [file]
-            y = fromMaybe no_parse $ fromRank [rank]
-
-        readLocation _ = no_parse
+    readsPrec _ _ = []
